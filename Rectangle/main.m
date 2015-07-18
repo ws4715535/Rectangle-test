14f39166d7d40ced120e0a28acfaa85978afab45
ws <ws@wsdeMacBook-Pro.local>
2015-07-18 22:23:42 +0800

    Initial Commit

---
+++ Rectangle/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Rectangle
+//
+//  Created by ws on 15-7-18.
+//  Copyright (c) 2015年 ws. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
