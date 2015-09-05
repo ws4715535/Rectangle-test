1f14223d6bddf274a42dcf70ef72b6bed081bc2e
ws <624412421@qq.com>
2015-09-05 23:10:47 +0800

    change 1.0
    this is the change first test

--- Rectangle/main.m
+++ Rectangle/main.m
@@ -7,11 +7,63 @@
 //
 
 #import <Foundation/Foundation.h>
+@interface Rectangle: NSObject;
+
+-(void) setWidth: (int) w;
+-(void) setHeigth: (int) h;
+-(int) width;
+-(int) heigth;
+-(int) area;
+-(int) perimeter;
+
+@end
+
+@implementation Rectangle;
+
+    int width;
+    int heigth;
+    int area;
+    int perimeter;
+
+-(void) setWidth:(int) w
+    {
+    width= w;
+    }
+
+-(void) setHeigth:(int) h
+    {
+    heigth= h;
+    }
+
+-(int) width
+    {
+    return width;
+    }
+
+-(int) heigth
+    {
+        return heigth;
+    }
+-(int) area
+    {
+        return area;
+    }
+-(int) perimeter
+{
+    return perimeter;
+    
+}
+@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Rectangle *rectangle1=[[Rectangle alloc]init];
+        [rectangle1 setWidth:3];
+        [rectangle1 setHeigth:5];   // 将4变为5
+        NSLog(@"area=%d",width*heigth);
+        NSLog(@"perimeter=%d",width*heigth*2);
+        NSLog(@"矩形的高为%d,矩形的宽为%d)",heigth,width);
+        
     }
     return 0;
 }
